8393c58b2c2b4bb439a993a94763feb2c94632c3
qifei <luoqf@yomob.com>
2018-08-02 15:57:21 +0800

    add min max sum

--- hackerrank/src/main/java/algorithms/MiniMaxNum.java
+++ hackerrank/src/main/java/algorithms/MiniMaxNum.java
@@ -1,5 +1,8 @@
 package algorithms;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -10,46 +13,48 @@ import java.util.Scanner;
  * 最大子集和，全部和减去最小值
  */
 public class MiniMaxNum {
-    static void miniMaxSum(int[] arr) {
-
-        int size = arr.length;
-        long sum = 0;
-        for (int a: arr) {
-            sum = sum+a;
-        }
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 1; j < size-i; j++) {
-                if(arr[j-1]>arr[j]){
-                    int tmp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = tmp;
-                }
-            }
-        }
-        for (int a: arr) {
-            System.out.println(a);
-        }
-        long min = sum - arr[size-1];
-        long max = sum - arr[0];
-        System.out.println(min+" "+max);
-
-    }
-
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        int[] arr = new int[5];
-
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < 5; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
+        static int birthdayCakeCandles(int[] ar) {
+            int max = ar[0];
+            for(int i = 0;i<ar.length;i++){
+                if(ar[i]>=max){
+                    max = ar[i];
+                }
+            }
+            int blowout = 0;
+            for (int i = 0;i<ar.length;i++) {
+                if(ar[i]==max){
+                    blowout++;
+                }
+            }
+            return blowout;
         }
 
-        miniMaxSum(arr);
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/Users/qifei/txt"));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+        System.out.println(result);
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
 
         scanner.close();
     }
